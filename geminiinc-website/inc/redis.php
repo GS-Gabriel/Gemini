400c07d799ca1ceb58bfda3b93dd294417b56ee9
Gabriel Sousa <104873711+GS-Gabriel@users.noreply.github.com>
2023-03-02 21:51:51 -0300

    Update redis.php

--- geminiinc-website/inc/redis.php
+++ geminiinc-website/inc/redis.php
@@ -1,7 +1,7 @@
 <?php
 
 $redis = new Redis();
-$redis->connect('127.0.0.1', 6379);
-$redis->auth('8a7b86a2cd89d96dfcc125ebcc0535e6');
+$redis->connect('geminiincredis.vujqre.ng.0001.sae1.cache.amazonaws.com', 6379);
+// $redis->auth('8a7b86a2cd89d96dfcc125ebcc0535e6');
 
 ?>
